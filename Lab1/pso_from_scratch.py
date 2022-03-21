dcf26feb5ef163f03a1d6fe8c5d9a9ae9e9b4848
WildFireCodes <fifada13@gmail.com>
2022-03-21 18:12:28 +0100

    Mean bug fixed - working bare bones variant

--- Lab1/pso_from_scratch.py
+++ Lab1/pso_from_scratch.py
@@ -13,10 +13,10 @@ class pso:
                                         'gauss pso': self.gauss_pso}
 
     def create_population(self, n_dims, birds_num = 16):
-        return np.array([np.random.uniform(self.spawn_x, self.spawn_y) for _ in range(n_dims) for _ in range(birds_num)]).reshape(-1, n_dims)
+        return np.random.uniform(self.spawn_x, self.spawn_y, size = (birds_num, n_dims)) #for _ in range(n_dims) for _ in range(birds_num)]).reshape(-1, n_dims)
 
     def bare_bones(self, pop_i, best_pop):
-        return np.random.normal((pop_i - best_pop)/2, np.abs(pop_i - best_pop)) #mean, sigma
+        return np.random.normal((pop_i + best_pop)/2, np.abs(pop_i - best_pop)) #mean, sigma
 
     def canonical_pso(self, pop_i, best_pop, prev_pop, velocity):
         '''vi - velocity, pi - prev_pop, xi - pop_i, pg - best_pop'''
@@ -32,35 +32,24 @@ class pso:
 
     def pso_engine(self, function, n_dims = 2, birds_num = 16, n_iters = 300, option = 'bare bones'):
         bird_pop = self.create_population(n_dims, birds_num)
-        best_bird = bird_pop[int(np.random.uniform(0, birds_num))] # randomowy ptaszek - pierwszy najlepszy
-        best_val = function(best_bird)
 
+        # tworze populacje -> wyniki dla populacji -> wartosc minimalna -> nowa populacja z nowymi wynikami -> czy sie przesuwa 
         for i in range(n_iters):
-            score = np.apply_along_axis(function, 1, bird_pop)
-            print(repr(score) + 'wynik')
-            print('\n')
-            best_idx = np.argsort(score)[0] # min
-            print(repr(best_idx) + 'index')
-            print('\n')
-            best_bird = bird_pop[best_idx] # min
-            best_val = function(best_bird)
-            print(repr(best_val) + 'best val')
-            print('\n')
+            bird_pop_score = np.apply_along_axis(function, 1, bird_pop)
+            best_bird = bird_pop[np.argmin(bird_pop_score)]
+            best_val_bird = function(best_bird)
 
-            print(repr(i) +'. Best_bird:'+ repr(best_bird) +'best val:'+ repr(best_val))
-            print('\n')
             next_bird_pop = self.update_functions_options[option](bird_pop, best_bird)
-            print(repr(next_bird_pop) + 'nowa populacja')
-            print('\n')
-            next_score = np.apply_along_axis(function, 1, next_bird_pop)#.reshape(1, birds_num)
-            print(repr(next_score) + 'nowy wynik')
-            print('\n')
-            print('\n')
-            #mask = next_score < score #zamiana tych lepszych
-            #bird_pop[mask] = next_bird_pop[mask]
+            # next_bird_pop = self.canonical_pso(bird_pop, best_bird, new_bird_pop, velocity)
+            next_bird_pop_score = np.apply_along_axis(function, 1, next_bird_pop)
 
-            if np.std(next_score) < 0.001:
-                print('STOP!')
+            #canonical pso
+            velocity = next_bird_pop - bird_pop
+                
+            mask = next_bird_pop_score < bird_pop_score #zamiana tych lepszych
+            bird_pop[mask] = next_bird_pop[mask]
+
+            if np.std(next_bird_pop_score) < 0.001:
                 break
         
-        return best_bird, best_val
+        return best_bird, best_val_bird
